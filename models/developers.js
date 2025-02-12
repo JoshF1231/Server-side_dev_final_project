c49ad0993e4ccc2d300001a6ddb91bdf080a7b54
RazHagani <raz4799@gmail.com>
2025-02-12 15:05:20 +0200

    Update developers.js

--- models/developers.js
+++ models/developers.js
@@ -1,3 +1,8 @@
+/**
+ * List of developers involved in the project.
+ * @type {Array<{first_name: string, last_name: string}>}
+ */
+
 const Developers = [
     {
         "first_name" : "Raz",
@@ -7,6 +12,12 @@ const Developers = [
         "first_name" : "Josh",
         "last_name" : "Fiquette"
     }]
+
+/**
+ * Retrieves all developers in the project.
+ * @returns {{data: Array<{first_name: string, last_name: string}>|null, err: Error|null}} 
+ * An object containing the list of developers or an error.
+ */
 
 function getAllDevelopers() {
     const result = {
