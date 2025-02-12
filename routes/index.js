7a7b1bf3c2e6fffeec2be9e20974f1194ac12170
RazHagani <raz4799@gmail.com>
2025-02-12 15:13:44 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/**
+ * GET route for the home page.
+ * @route GET /
+ * @group Home - Main application routes
+ * @param {Object} req - The request object
+ * @param {Object} res - The response object
+ * @param {Function} next - The next middleware function
+ * @returns {void} Renders the index page with a title
+ */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
