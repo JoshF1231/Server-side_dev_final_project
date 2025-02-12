7cf2affc11497abff119949c8fab599c7cb41bdf
RazHagani <raz4799@gmail.com>
2025-02-12 15:16:48 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,19 @@ const express = require('express');
 const router = express.Router();
 const { getCostsByUserId } = require('../models/costs');
 
-/* GET user costs */
+/**
+ * GET route to retrieve all costs for a specific user.
+ * @route GET /:userid
+ * @group Costs - Operations related to user expenses
+ * @param {Object} req - The request object
+ * @param {Object} req.params - The URL parameters
+ * @param {string} req.params.userid - The user ID for whom the costs are retrieved
+ * @param {Object} res - The response object
+ * @param {Function} next - The next middleware function
+ * @returns {Object} 200 - List of costs for the specified user
+ * @returns {Object} 500 - Internal server error (invalid user ID or system error)
+ */
+
 router.get('/:userid', async function(req, res, next) {
   try {
     const userid = req.params.userid;
